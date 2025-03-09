a6184df888ed81cca765c9ef6ede60060fbf4ef0
MatteoConcutelli <102553534+MatteoConcutelli@users.noreply.github.com>
2025-03-09 11:45:51 +0000

    Deploying to gh-pages from @ MatteoConcutelli/matteoconcutelli.github.io@67ed47c1b46533709fed4c8f6bf8b02f6baa51b5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -375,18 +375,12 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-",
-          title: '',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+    },{id: "news-ethical-hacking-linkedin-post",
+          title: 'Ethical-Hacking linkedin post',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{
+              window.location.href = "/news/announcement_1/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
